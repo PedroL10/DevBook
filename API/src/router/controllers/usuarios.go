4ace50d0ad9b89ff8b30c62048f911ca701e8602
PedroLGon <pepluisgon01@gmail.com>
2024-11-08 12:55:49 -0300

    Config dos DB e criacao do repositorio e controllers de usuarios

--- API/src/router/controllers/usuarios.go
+++ API/src/router/controllers/usuarios.go
@@ -1,9 +1,35 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
 
+// CriarUsuario insere um usuario no banco de dados - Controller(lida com as funções http, requisicoes e repostas)
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuario"))
+	corpoRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar() // Abre a conexao com o banco
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db) // Cria o repositorio e passa o banco pra dentro desse repositorio
+	repositorio.Criar(usuario)                                // Cria o usuario que vem na requisicao
+
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
